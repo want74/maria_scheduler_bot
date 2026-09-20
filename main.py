4bf6bcd0628614c5a5fe2d3ae15062a46f432b35
want74 <want74@rambler.ru>
2026-09-20 21:47:47 +0300

    FIX

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ USER_DATA_FILE = "user_groups.json"
 SUBSCRIPTIONS_FILE = "subscriptions.json"
 
 # время рассылки (локальное время сервера, 24-часовой формат)
-DAILY_SEND_HOUR = int(getenv("DAILY_SEND_HOUR", "21:47"))
-DAILY_SEND_MINUTE = int(getenv("DAILY_SEND_MINUTE", "0"))
+DAILY_SEND_HOUR = int(getenv("DAILY_SEND_HOUR", "21"))
+DAILY_SEND_MINUTE = int(getenv("DAILY_SEND_MINUTE", "48"))
 
 GROUPS_API = "https://ruz.guz.ru/api/dictionary/groups"
 SCHEDULE_API = "https://ruz.guz.ru/api/schedule/group/{group}"
